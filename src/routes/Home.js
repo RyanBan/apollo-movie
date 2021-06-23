33ab9798ac1027c51e433f6f70b5b5eec3f687eb
Ryan Ban <45275840+RyanBan@users.noreply.github.com>
2021-06-22 22:02:37 -0400

    Style component updated

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,7 +11,6 @@ const GET_MOVIES = gql`
             medium_cover_image
         }
     }
-
 `
 const Container = styled.div`
   display: flex;
@@ -44,19 +43,36 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
 
-export default () => {
+
+
+
+const Home = () => {
     const { loading, data } = useQuery(GET_MOVIES);
     return (
       <Container>
         <Header>
           <Title>Movie Flex</Title>
           {/* <Subtitle>I love GraphQL</Subtitle> */}
+          {console.log(loading, data)}
         </Header>
         {loading && <Loading>Loading...</Loading>}
-        {!loading &&
-          data.movies &&
-          data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+
+        <Movies>
+          {data?.movies?.map(m => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
       </Container>
     );
-}; 
+}
+
+export default Home
