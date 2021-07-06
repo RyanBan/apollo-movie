6bbb788aff9e1cf142de5bbb0725fe044d2c7ef9
Ryan Ban <45275840+RyanBan@users.noreply.github.com>
2021-07-06 14:48:15 -0400

    Mixed https error handled

--- src/apollo.js
+++ src/apollo.js
@@ -4,7 +4,7 @@ const cashe = new InMemoryCache();
 
 const client = new ApolloClient({
     cache: cashe,
-    uri: "http://movie-ql.herokuapp.com/"
+    uri: "https://movie-ql.herokuapp.com/"
 })
 
 export default client
